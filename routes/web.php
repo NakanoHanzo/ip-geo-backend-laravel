701ddaeb1de0d62d1fbf11c92aa5fadc52746bba
Marco Kucznierz <m.kucznierz@slenderiser.de>
2024-06-20 20:44:33 +0200

    register api route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IpLookupController;
+
+Route::post('/api/lookup', [IpLookupController::class, 'lookup']);
 
 Route::get('/', function () {
     return view('welcome');
